dfa8c2c12d5bc7d92d126f7efc7df726866e02a8
Guido Agapito <guido.agapito@inaf.it>
2026-04-02 08:15:18 +0200

    (GA) compute_gtilt_with_extrapolation with documentation and tests (#66)

--- test/test_intmat.py
+++ test/test_intmat.py
@@ -589,6 +589,7 @@ class TestIntmat(unittest.TestCase):
             dm_rotation=self.dm_rotation,
             gs_pol_coo=gs_pol_coo,
             gs_height=gs_height,
+            wfs_nsubaps=self.wfs_nsubaps,
             verbose=False,
             specula_convention=True,
         )
@@ -700,6 +701,7 @@ class TestIntmat(unittest.TestCase):
             dm_rotation=self.dm_rotation,
             gs_pol_coo=gs_pol_coo,
             gs_height=gs_height,
+            wfs_nsubaps=self.wfs_nsubaps,
             wfs_rotation=wfs_rotation,
             wfs_translation=wfs_translation,
             wfs_mag_global=wfs_mag_global,
